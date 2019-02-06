a1af45c3306dd41d3774ce77078db9fd845e026f
Richard Lea <chigix@zoho.com>
2019-02-07 03:20:04 +0900

    intro: nlp case with japan energy plan document dataset
    Signed-off-by: Richard Lea <chigix@zoho.com>

---
+++ data-raw/samples.R
@@ -0,0 +1,50 @@
+# Japanese Text Data source:
+# https://www.aozora.gr.jp/
+# Japan Energy Plan Prerelease
+download.file("http://www.enecho.meti.go.jp/committee/council/basic_policy_subcommittee/pdf/basic_policy_subcommittee_002.pdf",
+              "tmp.pdf")
+pages <- pdftools::pdf_text("tmp.pdf")
+readr::write_rds(
+  c(
+    pages[1:2],
+    stringr::str_remove_all(
+      unlist(
+        strsplit(
+          stringr::str_replace_all(
+            stringr::str_c(pages[3:105], collapse = ""),
+            "\n\\s+\\d+\n",
+            "\n"
+          ),
+          "\n "
+        )
+      ),
+      stringr::fixed("\n")
+    ),
+    pages[106:length(pages)]
+  ),
+  "./data/japan_energy_plan_pre.rds"
+)
+
+# Japan Energy Plan
+download.file("http://www.meti.go.jp/press/2018/07/20180703001/20180703001-1.pdf",
+              "tmp.pdf")
+pages <- pdftools::pdf_text("tmp.pdf")
+readr::write_rds(
+  c(
+    pages[1:2],
+    stringr::str_remove_all(
+      unlist(
+        strsplit(
+          stringr::str_replace_all(
+            stringr::str_c(pages[3:length(pages)], collapse = ""),
+            "\n\\s+\\d+\n",
+            "\n"
+          ),
+          "\n "
+        )
+      ),
+      stringr::fixed("\n")
+    )
+  ),
+  "./data/japan_energy_plan.rds"
+)
