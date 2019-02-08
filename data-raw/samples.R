9590e3035666ae127ff49788a4e2842a0fa4d104
Richard Lea <chigix@zoho.com>
2019-02-08 11:09:31 +0900

    chore: use tempfile path
    Signed-off-by: Richard Lea <chigix@zoho.com>

--- data-raw/samples.R
+++ data-raw/samples.R
@@ -2,8 +2,8 @@
 # https://www.aozora.gr.jp/
 # Japan Energy Plan Prerelease
 download.file("http://www.enecho.meti.go.jp/committee/council/basic_policy_subcommittee/pdf/basic_policy_subcommittee_002.pdf",
-              "tmp.pdf")
-pages <- pdftools::pdf_text("tmp.pdf")
+              tempfile("jp_energy_draft.pdf"))
+pages <- pdftools::pdf_text(tempfile("jp_energy_draft.pdf"))
 readr::write_rds(
   c(
     pages[1:2],
@@ -27,8 +27,8 @@ readr::write_rds(
 
 # Japan Energy Plan
 download.file("http://www.meti.go.jp/press/2018/07/20180703001/20180703001-1.pdf",
-              "tmp.pdf")
-pages <- pdftools::pdf_text("tmp.pdf")
+              "jp_energy.pdf")
+pages <- pdftools::pdf_text("jp_energy.pdf")
 readr::write_rds(
   c(
     pages[1:2],
